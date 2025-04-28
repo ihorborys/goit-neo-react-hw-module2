1c1bc5f699a752ba98505df862139e24e9d70b3f
ihorborys <ihorborys@gmail.com>
2025-04-28 14:38:51 +0300

    Init commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(
+      {
+          good: 0,
+          neutral: 0,
+          bad: 0
+      }
+  )
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
